84bc1664868b4415d352d4f4c08a40f047445d19
Malaxor <nogobaduk@gmail.com>
2019-03-15 16:06:49 -0700

    function expressions/statements

---
+++ 4-Modules, Exports, and Require/app.js
@@ -0,0 +1,19 @@
+
+// function statement 
+function greet() {
+   console.log('Hello');
+}
+greet();
+
+// example of first class function
+function logGreeting(fn) {
+   fn();
+}
+logGreeting(() => {
+   console.log('Hello, again');
+});
+
+// function expression
+const greetMe = function() {
+   console.log('Yo, guy!');
+}();
