dff41d8f5295181f8a1ac4a92d6c1382f1d7de8f
Philip <nogobaduk@gmail.com>
2019-04-02 20:18:27 -0700

    outputting html and templetes

---
+++ 7-HTTP and Being a Web Server/60-Outputting HTML and Templates/app.js
@@ -0,0 +1,14 @@
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+
+   const message = 'Hello, there...';
+   html = html.replace('{Message}', message);
+
+   res.end(html);
+}).listen(1337, '127.0.0.1');
