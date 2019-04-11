0629f94eccde76a70cd2f01233d1d0dca2dc8bca
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-04-11 09:07:08 -0700

    npm: init, nodemon and package.json

--- 7-HTTP and Being a Web Server/64-Routing/app.js
+++ 7-HTTP and Being a Web Server/64-Routing/app.js
@@ -2,13 +2,11 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(req, res) {
-
    if(req.url === '/') {
       fs.createReadStream(__dirname + '/index.html').pipe(res);
    }
    else if(req.url === '/api') {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-   
       const obj = {
          firstName: 'John',
          lastName: 'Doe'
