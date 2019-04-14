5224369005b1d1805f5e0f29c484a1d3cc1d8941
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-04-14 15:30:47 -0700

    template and template engines

--- 9-Express/76-Templates_and_Template_Engines/app.js
+++ 9-Express/76-Templates_and_Template_Engines/app.js
@@ -4,20 +4,21 @@ const port = process.env.PORT || 3000;
 // express.static makes static (not dynamic) files available on request
 // this is middlewear (between req and res)
 app.use('/assets', express.static(__dirname + '/public'));
+// template setup
+app.set('view engine', 'ejs');
 // if you omit the route ('/'), the middlewear will be applied to all routes
 app.use('/', (req, res, next) => {
    console.log(`Request URL: ${req.url}`);
    //call the next middlewear that is some way connected to this particular route
    next();
 });
-
 app.get('/', (req, res) => {
-   res.send('<html><head><link href=assets/style.css type=text/css rel="stylesheet"/></head><body><h1>Hello, world</h1></body></html>');
+   res.render('index');
 });
 app.get('/api', (req, res) => {
    res.json({ firstName: 'John', lastName: 'Doe' });
 });
 app.get('/person/:id', (req, res) => {
-   res.send(`<html><head></head><body><h1>Hello, ${req.params.id}</h1></body></html>`);
+   res.render('person', { id: req.params.id });
 });
 app.listen(port);
